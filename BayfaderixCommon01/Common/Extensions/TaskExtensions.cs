f7caeb8f18260291f14adc8949883085c9854d67
Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>
2023-02-04 08:23:16 +0300

    Small refactor of stuff, added single threaded synchronization context with some probably bad tweaks. V 2.0.1 ig
    Signed-off-by: Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>

--- BayfaderixCommon01/Common/Extensions/TaskExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Name.Bayfaderix.Darxxemiyur.Common.Extensions
-{
-	public static class TaskExtensions
-	{
-		public static async Task<TResult?> AsType<TInput, TResult>(this Task<TInput> task) where TInput : class where TResult : class => await task.ConfigureAwait(false) as TResult;
-	}
-}
