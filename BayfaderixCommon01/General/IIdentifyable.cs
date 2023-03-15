a370d0ccac1fee12095b3c0ab7fc35c02bf2eca2
Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>
2023-03-15 20:21:39 +0300

    Added interfaces/contracts for identifyable types.
    Signed-off-by: Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>

---
+++ BayfaderixCommon01/General/IIdentifyable.cs
@@ -0,0 +1,32 @@
+﻿namespace Name.Bayfaderix.Darxxemiyur.General
+{
+	/// <summary>
+	/// Provides a wrapper for a type that should be identifyable.
+	/// </summary>
+	/// <typeparam name="TIdentifyable">A wrappable object.</typeparam>
+	public interface IIdentifyable<TIdentifyable>
+	{
+		/// <summary>
+		/// Identity.
+		/// </summary>
+		IIdentity? Identity {
+			get;
+		}
+
+		/// <summary>
+		/// The contained type.
+		/// </summary>
+		TIdentifyable? Identifyable {
+			get;
+		}
+
+		/// <summary>
+		/// Compares two instances for equality.
+		/// </summary>
+		/// <param name="to"></param>
+		/// <returns>
+		/// True if they are equal according to the implementation of the interface. False otherwise.
+		/// </returns>
+		bool Equals(IIdentifyable<TIdentifyable> to);
+	}
+}
---
+++ BayfaderixCommon01/General/IIdentity.cs
@@ -0,0 +1,17 @@
+﻿namespace Name.Bayfaderix.Darxxemiyur.General
+{
+	/// <summary>
+	/// Identity interface.
+	/// </summary>
+	public interface IIdentity
+	{
+		/// <summary>
+		/// Compares two instances for equality.
+		/// </summary>
+		/// <param name="to"></param>
+		/// <returns>
+		/// True if they are equal according to the implementation of the interface. False otherwise.
+		/// </returns>
+		bool Equals(IIdentity to);
+	}
+}
