b7234ce32e0587f7d079037ae4fdd12e0d0440ec
Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>
2023-04-17 23:02:04 +0300

    Added nullable extensions
    Signed-off-by: Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>

---
+++ BayfaderixCommon01/Extensions/NullableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Name.Bayfaderix.Darxxemiyur.Extensions;
+
+public static class NullableExtensions
+{
+	public static T ThrowIfNull<T>(
+		this T? input, [CallerArgumentExpression("input")] string? description = null)
+		where T : struct =>
+		input ?? ThrowMustNotBeNull<T>(description);
+
+	public static T ThrowIfNull<T>(
+		this T? input, [CallerArgumentExpression("input")] string? description = null)
+		where T : class =>
+		input ?? ThrowMustNotBeNull<T>(description);
+
+	private static T ThrowMustNotBeNull<T>(string? description) =>
+		throw new InvalidOperationException($"{description} must not be null");
+}
